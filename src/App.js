3408be61bce6e640a057347927ecd00c8f48f969
Hafeez Ali <hafeez.sayed786@gmail.com>
2022-05-31 02:57:32 +0530

    Quiz app

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import './App.css';
+import SetupForm from './Form/SetupForm';
+import Modal from './Modal/Modal';
+import LoadingScreen from './Loading/LoadingScreen';
+import { useGlobalContext } from './Context/Context';
+
+function App() {
+  const {
+    waiting,
+    loading,
+    questions,
+    index,
+    correct,
+    nextQuestions,
+    checkAnswers,
+  } = useGlobalContext();
+
+  if(waiting) {
+    return(
+      <SetupForm />
+    )
+  }
+
+  if(loading) {
+    return <LoadingScreen />
+  }
+
+  const { incorrect_answers, correct_answer, question } = questions[index];
+  let answers = [...incorrect_answers];
+  const tempIndex = Math.floor(Math.random() * 4);
+  if (tempIndex === 3) {
+    answers.push(correct_answer);
+  } else {
+    answers.push(answers[tempIndex]);
+    answers[tempIndex] = correct_answer;
+  }
+
+  return (
+    <main>
+      <Modal />
+      <section className="quiz">
+        {console.log(questions)}
+        <p className="correct-answers">
+          correct answers: {correct}/{index}
+        </p>
+        <article className="container">
+          <h2 dangerouslySetInnerHTML={{ __html: question }} />
+          <div className="btn-container">
+            {answers.map((answer, index) => {
+              return (
+                <button
+                  key={index}
+                  className="answer-btn"
+                  onClick={() => checkAnswers(correct_answer === answer)}
+                  dangerouslySetInnerHTML={{ __html: answer }}
+                />
+              );
+            })}
+          </div>
+        </article>
+        <button className="next-question" onClick={nextQuestions}>
+          next questions
+        </button>
+      </section>
+    </main>
+  )
+}
+
+export default App;
---
+++ src/Loading/LoadingScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const LoadingScreen = () => {
+  return (
+    <main className='loading'>
+    </main>
+  )
+}
+
+export default LoadingScreen
